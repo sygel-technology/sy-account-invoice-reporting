046c330e81652e196a099f2fe5d2555c96df5d4a
Manuel Florido <manuel.florido@sygel.es>
2026-09-10 15:58:57 +0200

    [IGN] test commit

--- account_invoice_report_commercial_address/__manifest__.py
+++ account_invoice_report_commercial_address/__manifest__.py
@@ -4,13 +4,14 @@
 {
     "name": "Account Invoice Report Commercial Address",
     "summary": "Add commercial address to invoice report",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "license": "AGPL-3",
     "author": "Sygel",
     "category": "Accounting & Finance",
     "website": "https://github.com/sygel-technology/sy-account-invoice-reporting",
     "depends": [
         "account",
+        "base",
     ],
     "data": [
         "views/account_move_views.xml",
